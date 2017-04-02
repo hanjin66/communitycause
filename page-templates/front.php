435aa7520bbdac72fc6ec1adb7f8af0b5114c13c
David Diomede <diomededavid@gmail.com>
2017-04-01 21:56:14 -0700

    Moved button down

--- page-templates/front.php
+++ page-templates/front.php
@@ -11,22 +11,22 @@ get_header(); ?>
 		    <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
 		    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
 		    <li class="is-active orbit-slide">
-		      <img class="orbit-image" src="http://placehold.it/1920x600" alt="Space">
+		      <img class="orbit-image" src="http://placehold.it/1920x500" alt="Space">
 		    </li>
 		    <li class="orbit-slide">
-		      <img class="orbit-image" src="http://placehold.it/1920x600" alt="Space">
+		      <img class="orbit-image" src="http://placehold.it/1920x500" alt="Space">
 		    </li>
 		    <li class="orbit-slide">
-		      <img class="orbit-image" src="http://placehold.it/1920x600" alt="Space">
+		      <img class="orbit-image" src="http://placehold.it/1920x500" alt="Space">
 		    </li>
 		    <li class="orbit-slide">
-		      <img class="orbit-image" src="http://placehold.it/1920x600" alt="Space">
+		      <img class="orbit-image" src="http://placehold.it/1920x500" alt="Space">
 		    </li>
 		  </ul>
 	 	</div>
 	</div>
 	<row>
-		<div class="large expanded button-group">
+		<div class="small expanded button-group">
 		  <a class="button warning">Donate</a>
 		  <a class="button success">Volunteer</a>
 		  <a class="button secondary">Events</a>
@@ -34,6 +34,44 @@ get_header(); ?>
 	</row>
 </header>
 
+<section class="benefits">
+	<div class="row">
+		<div class="columns">
+			<h1>HOW WE CAN HELP YOU</h1>
+		</div>
+	</div>
+	
+	
+		<?php query_posts( 'category_name=special-cat&posts_per_page=12' ); ?>
+		<?php if ( have_posts() ) : ?>
+	    <?php while ( have_posts() ) : the_post(); 
+		    $i = $wp_query->current_post;
+            echo ( 0 == $i % 4 ) ? '<div class="row">' : '';
+	    ?>
+	  	<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'small' );?>			
+			
+			<div class="small-12 large-3 columns">
+				<div class="card" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat; background-size: cover;">
+			        <div class="title">
+			        	<h5 class="text-left">
+			        	    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+			        	</h5>
+			        </div>
+		        </div>
+		        <div class="expanded button-group">
+						<a class="primary button" href="<?php the_permalink() ?>">View</a>
+					</div> 
+        	</div>
+		<?php echo ( $wp_query->post_count == $i || 3 == $i % 4 ) ? '</div>' : '';
+        endwhile; ?>
+	 	<?php endif; ?>
+	
+</section>
+
+
+
+
+<!--
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <section class="intro" role="main">
@@ -66,72 +104,15 @@ get_header(); ?>
 </section>
 <?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
+-->
 
 
 
-<section class="benefits">
-	<div class="row">
-		<div class="columns">
-			<h1>HOW WE CAN HELP YOU</h1>
-		</div>
-	</div>
-	<div class="row">
-		<?php if ( have_posts() ) : ?>
-	    <?php while ( have_posts() ) : the_post(); ?>
-	  	<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'small' );?>			
-			
-			<div class="small-12 large-3 columns">
-				<div class="card" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat; background-size: cover;">
-			        <div class="title">
-			        	<h5 class="text-left">
-			        	    <a href="#"><?php the_title(); ?></a>
-			        	</h5>
-			        </div>
-			        <div class="expanded button-group">
-						<a class="primary button">View</a>
-					</div>  
-		        </div>
-        </div>
-		<?php endwhile; ?>
-	 	<?php endif; ?>
-	
-	
-	
-	
-        	
-		<div class="small-12 large-3 columns">
-			<div class="card" style="background: url('http://placehold.it/350x350') no-repeat; background-size: cover;">
-				
-				<h3><?php the_title(); ?></h3>
-				<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
-				<div class="button-group">
-				  <a class="secondary button">View</a>
-				</div>	
-			</div>
-		</div>
-		<div class="small-12 large-3 columns">
-			<div class="card" style="background: url('http://placehold.it/350x350') no-repeat; background-size: cover;">
-				
-				
-				<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
-				<div class="button-group">
-				  <a class="secondary button">View</a>
-				</div>	
-			</div>
-		</div>
-		<div class="small-12 large-3 columns">
-			<div class="card" style="background: url('http://placehold.it/350x350') no-repeat; background-size: cover;">
-				
-				<h3><?php the_title(); ?></h3>
-				<p>Everything is semantic. You can have the cleanest markup without sacrificing the utility and speed of Foundation.</p>
-				<div class="button-group">
-				  <a class="secondary button">View</a>
-				</div>	
-			</div>
-		</div>
-	</div>
 
-</section>
+
+
+
+
 
 
 
