7f1cdc2c22637c4c529402dc3710c522fa82e63d
David Diomede <diomededavid@gmail.com>
2017-04-14 08:32:57 -0700

    Added front-page layout based on Eileens wireframes

---
+++ front-page.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+<header role="banner">
+	
+	<div class="row">
+		<div class="columns">
+		<h1 class="text-center">WE HELP PEOPLE DO GOOD</h1>
+	</div>
+	
+	<div class="row collapse">
+		<div class="small-12 medium-6 large-6 columns">
+			  <img src="http://placehold.it/820x620/" alt="">
+		</div>
+		
+		<div class="small-12 medium-6 large-6 columns">
+			<div class="row collapse">
+			  	<img src="http://placehold.it/820x310/666" alt="">
+			</div>
+			<div class="row collapse">
+			  	<img src="http://placehold.it/820x310/555" alt="">
+			</div>
+		</div>
+	</div>
+</header>
+
+<section role="benefits">
+	<div class="row">
+		<div class="columns">
+			<h2 class="text-center">WE CAN HELP YOU</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea suscipit, amet assumenda nostrum modi excepturi, vel quos quas, eos et commodi odio culpa soluta! Repellendus temporibus illum perspiciatis! Tenetur, maxime!</p>
+		</div>
+	</div>
+	<div class="row small-up-1 medium-up-3 large-up-3">
+		<div class="column column-block">
+			<img src="//placehold.it/600x600" class="thumbnail" alt="">
+			<a class="hollow button expanded" href="#">Such Expand</a>
+		</div>
+		<div class="column column-block">
+			<img src="//placehold.it/600x600" class="thumbnail" alt="">
+			<a class="hollow button expanded" href="#">Such Expand</a>
+		</div>
+		<div class="column column-block">
+		<img src="//placehold.it/600x600" class="thumbnail" alt="">
+			<a class="hollow button expanded" href="#">Such Expand</a>
+		</div>
+	</div>			
+</section>
+
+<section>
+	<div class="row">
+		<div class="columns">
+			<h2 class="text-center">ABOUT US</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea suscipit, amet assumenda nostrum modi excepturi, vel quos quas, eos et commodi odio culpa soluta! Repellendus temporibus illum perspiciatis! Tenetur, maxime!</p>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="columns">
+			<ul class="tabs" data-tabs id="example-tabs">
+			  <li class="tabs-title is-active"><a href="#panel1" aria-selected="true">OUR MISSION</a></li>
+			  <li class="tabs-title text-center"><a href="#panel2">OUR BELIEFS</a></li>
+			  <li class="tabs-title"><a href="#panel3">OUR VISION</a></li>
+			</ul>
+			<div class="tabs-content" data-tabs-content="example-tabs">
+				<div class="tabs-panel is-active" id="panel1">
+					<p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>
+				</div>
+				<div class="tabs-panel" id="panel2">
+					<p>Suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+				</div>
+				<div class="tabs-panel" id="panel3">
+					<p>Suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="large-6 large-offset-3 columns">
+			<a class="hollow button expanded" href="#">MORE</a>
+		</div>
+	</div>
+	
+</section>
+
+<section role="events">
+	<div class="row">
+		<div class="columns">
+			<h2 class="text-center">WE CAN HELP YOU</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea suscipit, amet assumenda nostrum modi excepturi, vel quos quas, eos et commodi odio culpa soluta! Repellendus temporibus illum perspiciatis! Tenetur, maxime!</p>
+		</div>
+	</div>
+	<div class="row">
+			<div class="small-6 medium-8 large-8 columns">
+				<div class="row">
+					<div class="small-6 medium-6 large-6 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x400/666">
+						</div>
+					</div>
+					<div class="small-6 medium-6 large-6 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x400/666">
+						</div>	
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="small-6 medium-6 large-6 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x400/666">
+						</div>
+					</div>
+					<div class="small-6 medium-6 large-6 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x400/666">
+						</div>	
+					</div>
+				</div>
+			</div>
+			
+			<div class="small-12 medium-4 large-4 columns">
+				<div class="row">
+					<div class="small-12 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x157/666">
+						</div>
+					</div>
+					<div class="small-12 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x157/666">
+						</div>	
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="small-12 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x157/666">
+						</div>
+					</div>
+					<div class="small-12 columns">
+						<div class="thumbnail">
+							<img src="http://placehold.it/600x157/666">
+						</div>	
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="small-12 columns">
+							<a class="hollow button expanded" href="#">MORE EVENTS ></a>
+					</div>
+					
+				</div>
+			</div>
+		
+		
+	</div>			
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer();
