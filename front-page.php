a21f8c44a066c13fe512573d0873559d66f70970
David Diomede <diomededavid@gmail.com>
2017-04-16 18:41:15 -0700

    added background styles

--- front-page.php
+++ front-page.php
@@ -171,15 +171,4 @@ get_header(); ?>
 </section>
 
 
-
-
-
-
-
-
-
-
-
-
-
 <?php get_footer();
--- page-templates/front.php
+++ page-templates/front.php
@@ -11,7 +11,11 @@ get_header(); ?>
 		    <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
 		    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
 		    <li class="is-active orbit-slide">
+		    <?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 		      <img class="orbit-image" src="http://placehold.it/1920x500" alt="Space">
+		      <?php endwhile; ?>
+<?php endif; ?>
 		    </li>
 		    <li class="orbit-slide">
 		      <img class="orbit-image" src="http://placehold.it/1920x500" alt="Space">
